e4c3dc4a888e7385ecca4d90f4cccd3be73350cb
alecallain <alecjallain@gmail.com>
2024-11-10 21:33:00 -0500

    Adding base path into main file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<a href="{base}/">Home</a>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
