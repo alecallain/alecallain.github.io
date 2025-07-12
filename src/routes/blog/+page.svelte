f7e5253106b03a7e13d24f770d2251b8682c025e
alecallain <alecallain@gmail.com>
2025-07-12 17:43:28 -0400

    Apparently need base for slug paths, who wouldve thunk it

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,12 @@
-<script></script>
+<script>
+    import { base } from '$app/paths';
+</script>
 
 <div class="routeContent">
     <h1>Blog</h1>
     <p>Here is where you can check out my latest blog posts</p>
     <ul>
-        <li><a href="/blog/one">One Blog</a></li>
-        <li><a href="/blog/two">Two Blog</a></li>
+        <li><a href="{base}/blog/one">One Blog</a></li>
+        <li><a href="{base}/blog/two">Two Blog</a></li>
     </ul>
 </div>
